4501eb97c2108cfe2f6960c6f4223138778ca2dc
Francisco Rossi <franciscorossi@Franciscos-MacBook-Pro.local>
2019-12-15 14:00:52 -0300

    same

--- operaciones.c
+++ operaciones.c
@@ -398,13 +398,17 @@ void RTS(mos6502_t *p_mos){
 
 void SBC(mos6502_t *p_mos){
 
-	uint16_t aux = p_mos->a + *(p_mos->inst->m);
+	uint16_t aux = p_mos->a - *(p_mos->inst->m);
 	set_overflow(&(p_mos->status), p_mos->a, *(p_mos->inst->m), aux);
 	uint16_t aux2 = aux - (!get_status((&p_mos->status),CARRY));
 	set_overflow(&(p_mos->status), aux, -(!get_status((&p_mos->status),CARRY)), aux);
 	set_carry (&(p_mos->status), ~p_mos->a);
 
-	p_mos->a = aux2;
+	/*uint16_t aux = p_mos->a - *(p_mos->inst->m)- (!get_status((&p_mos->status),CARRY));
+	set_overflow(&(p_mos->status), p_mos->a, *(p_mos->inst->m)-, aux);
+	set_carry (&(p_mos->status), aux);*/
+
+	p_mos->a = aux;
 
 	set_zero (&(p_mos->status), p_mos->a);
 	set_negative (&(p_mos->status), p_mos->a);
